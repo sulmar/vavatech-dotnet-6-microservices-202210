a16f3a7ef0a28c2ea927f80d0ff128af49bb788b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-04 10:30:29 +0200

    Generyczne repozytorium

---
+++ src/Shopper/Core.Domain/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public interface IEntityRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        IEnumerable<TEntity> Get();
+        TEntity Get(int id);
+        bool IsExists(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(int id);
+    }
+}
---
+++ src/Shopper/Core.Infrastructure/InMemoryEntityRepository.cs
@@ -0,0 +1,50 @@
+﻿using Core.Domain;
+
+namespace Core.Infrastructure
+{
+    public abstract class InMemoryEntityRepository<TEntity> : IEntityRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly IList<TEntity> _entities;
+        
+        public virtual void Add(TEntity entity)
+        {
+            int id = _entities.Max(p => p.Id);
+
+            entity.Id = ++id;
+
+            _entities.Add(entity);
+        }
+
+        public virtual IEnumerable<TEntity> Get()
+        {
+            return _entities;
+        }
+
+        public virtual TEntity Get(int id)
+        {
+            return _entities.SingleOrDefault(p => p.Id == id);
+        }
+
+        public virtual bool IsExists(int id)
+        {
+            return _entities.Any(p => p.Id == id);
+        }
+
+        public virtual void Remove(int id)
+        {
+            _entities.Remove(Get(id));
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            var id = entity.Id;
+
+            Remove(id);
+
+            Add(entity);
+
+            entity.Id = id;
+        }
+    }
+}
