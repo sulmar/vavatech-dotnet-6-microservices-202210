5bdb4d9ed7a96aef821194fd175a7cc5acfee05f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-05 13:55:18 +0200

    gRPC

---
+++ src/Shopper/WarehouseClient.Receiver/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System.ComponentModel.DataAnnotations;
+using WarehouseService.Api;
+
+Console.WriteLine("Hello, gRPC Receiver!");
+
+
+const string url = "https://localhost:5051";
+
+using var channel = GrpcChannel.ForAddress(url);
+
+var client =  new WarehouseService.Api.WarehouseService.WarehouseServiceClient(channel);
+
+var request = new WarehouseService.Api.GetProductStateRequest { ProductId = 1, Color = "Red" };
+
+var response = await client.GetProductStateAsync(request);
+
+Console.WriteLine($"{response.IsAvailable} {response.Quantity}" );
+
+ var subscription = client.SubscribeProductState(request);
+
+var productStates = subscription.ResponseStream.ReadAllAsync();
+
+await foreach (GetProductStateResponse productState in productStates)
+{
+    Console.WriteLine($"{productState.IsAvailable} {productState.Quantity}");
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+
+
+
+
+
+
---
+++ src/Shopper/WarehouseService.Api/Services/MyWarehouseService.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using Grpc.Core;
+
+namespace WarehouseService.Api.Services
+{
+    public class MyWarehouseService : WarehouseService.WarehouseServiceBase
+    {
+        public override Task<GetProductStateResponse> GetProductState(GetProductStateRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"{request.ProductId} {request.Color}");
+
+            var response = new GetProductStateResponse { IsAvailable = true, Quantity = 10 };
+
+            return Task.FromResult(response);
+        }
+
+        public override async Task SubscribeProductState(GetProductStateRequest request, IServerStreamWriter<GetProductStateResponse> responseStream, ServerCallContext context)
+        {
+            // dotnet add package Bogus
+            var responses = new Faker<GetProductStateResponse>()
+                .RuleFor(p => p.Quantity, f => f.Random.Int(0, 100))
+                .RuleFor(p => p.IsAvailable, (f, response) => response.Quantity > 0)
+                .GenerateForever();
+
+            foreach (var response in responses)
+            {
+                await responseStream.WriteAsync(response);
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
